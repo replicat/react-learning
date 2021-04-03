d41e0b7193d8453a08939faf2343c23f4c32d750
Lucas Lee <replicat@users.noreply.github.com>
2021-04-03 20:04:55 +0800

    change all css to scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css'
+import './App.scss'
 import Main from './components/Main'
 import Header from './components/Header'
 import Footer from './components/Footer'
