3d94177c2e24d5236f0b6e2a8413dcf637c48948
Lucas Lee <replicat@users.noreply.github.com>
2021-04-04 08:52:43 +0800

    add card on magic link

--- src/components/MagicLink.js
+++ src/components/MagicLink.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Card, Button, InputGroup } from '@blueprintjs/core';
+import "./MagicLink.scss";
 
 class MagicLink extends React.Component {
     render() {
         return (
-            <div>This is the Magic Link page.</div>
+            <div className="magic-link">
+                <Card>
+                    <p>This is the Magic Link page</p>
+                    <InputGroup />
+                    <Button text="Generate Magic Link" />
+                </Card>
+            </div>
         )
     }
 }
