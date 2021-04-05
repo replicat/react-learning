1f52285d84715010715831f1c0a115cab12c1110
Lucas Lee <replicat@users.noreply.github.com>
2021-04-06 04:58:09 +0800

    update header datetime

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Header.scss'
 import { ReactComponent as LogoSvg } from '../assets/cryptodotcom-logo.svg';
 
 export default function Header() {
+    const [datetime, setDatetime] = useState(new Date().toLocaleString());
+
+    useEffect(() => {
+        let secTimer = setInterval(() => {
+            setDatetime(new Date().toLocaleString())
+        }, 1000)
+
+        return () => clearInterval(secTimer);
+    }, []);
+
     return (
         <header>
             <div className="logo"><LogoSvg /></div>
             <div className="title">QA Toolbox</div>
-            <div className="datetime">01/01/2021 00:00 (GMT+8)</div>
+            <div className="datetime">{datetime}</div>
         </header>
     )
 }
