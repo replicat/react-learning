856703b5349a9a69154f1aef2386125801aae297
Lucas Lee <replicat@users.noreply.github.com>
2021-04-06 04:51:38 +0800

    add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Header.scss'
+import { ReactComponent as LogoSvg } from '../assets/cryptodotcom-logo.svg';
 
 export default function Header() {
     return (
         <header>
+            <div className="logo"><LogoSvg /></div>
             <div className="title">QA Toolbox</div>
             <div className="datetime">01/01/2021 00:00 (GMT+8)</div>
         </header>
